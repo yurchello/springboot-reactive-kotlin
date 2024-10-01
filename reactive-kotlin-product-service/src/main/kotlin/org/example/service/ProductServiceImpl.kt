03a663fe063fe28993c884dfc5145aa6696e44a1
Yuriy_Mezentsev <yuriy_mezentsev@epam.com>
2024-10-01 17:25:21 +0200

    mapper support added

--- reactive-kotlin-product-service/src/main/kotlin/org/example/service/ProductServiceImpl.kt
+++ reactive-kotlin-product-service/src/main/kotlin/org/example/service/ProductServiceImpl.kt
@@ -9,26 +9,27 @@ import org.example.mapper.ProductMapper
 import org.springframework.stereotype.Service
 
 @Service
-class ProductServiceImpl : ProductService{
+class ProductServiceImpl(
+        private val productMapper: ProductMapper) : ProductService {
     override suspend fun getById(id: Long): ProductDto {
-
-        return ProductDto(1,"test", 3.4)
+        val productDto = productMapper.toDto(Product(1,"ttt", 5.5))
+        return ProductDto(1, "test", 3.4)
     }
 
     override suspend fun getAll(): Flow<ProductDto> {
         return flow {
             repeat(6) { it ->
-                emit(ProductDto(1,"test", 3.4))
+                emit(ProductDto(1, "test", 3.4))
                 delay(3000)
             }
         }
     }
 
     override suspend fun save(productDto: ProductDto): ProductDto {
-        return ProductDto(1,"test", 3.4)
+        return ProductDto(1, "test", 3.4)
     }
 
     override suspend fun update(id: Long, productDto: ProductDto): ProductDto {
-        return ProductDto(1,"test", 3.4)
+        return ProductDto(1, "test", 3.4)
     }
 }
